37fd1af87a085a2ec1909bfa1985e36149110047
ValeriiaKut <valeriiakutsenkoo@gmail.com>
2024-12-21 19:15:32 +0100

    Nowa zmiana

---
+++ lib/views/notes/notes_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class NotesView extends StatelessWidget {
+  const NotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Notes'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+          },
+        ),
+      ),
+    );
+  }
+}
